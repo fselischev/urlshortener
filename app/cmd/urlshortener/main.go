032b5bbc9ec26c928f2013d05d40b926bdc76cda
Fedor Selischev <yanerala@gmail.com>
2024-05-07 23:12:32 +0300

    prometheus metrics collection added

--- app/cmd/urlshortener/main.go
+++ app/cmd/urlshortener/main.go
@@ -1,5 +1,3 @@
-//go:build !solution
-
 package main
 
 import (
@@ -8,8 +6,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 	"urlshortener/internal/storage"
 	"urlshortener/pkg/shortener"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	endpointRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "url_shortener_endpoint_requests_total",
+		Help: "Total number of requests to each endpoint",
+	}, []string{"endpoint", "client"})
+
+	endpointDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "url_shortener_endpoint_duration_seconds",
+		Help:    "Duration of requests to each endpoint",
+		Buckets: prometheus.DefBuckets,
+	}, []string{"endpoint"})
 )
 
 func main() {
@@ -23,8 +39,29 @@ func main() {
 	shortener := shortener.New(db)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /shorten", shortener.ShortenHandler)
-	mux.HandleFunc("GET /go/", shortener.GoHandler)
+	mux.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		defer func() {
+			duration := time.Since(startTime).Seconds()
+			endpointDuration.WithLabelValues("/shorten").Observe(duration)
+		}()
+
+		endpointRequests.WithLabelValues("/shorten", "client").Inc()
+		shortener.ShortenHandler(w, r)
+	})
+
+	mux.HandleFunc("/go/", func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		defer func() {
+			duration := time.Since(startTime).Seconds()
+			endpointDuration.WithLabelValues("/go/").Observe(duration)
+		}()
+
+		endpointRequests.WithLabelValues("/go/", "client").Inc()
+		shortener.GoHandler(w, r)
+	})
+
+	mux.Handle("/metrics", promhttp.Handler())
 
 	portFlag := flag.Int("port", 6029, "port number")
 	flag.Parse()
